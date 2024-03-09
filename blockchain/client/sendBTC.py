44fcd12e0fd896923cfab1b325b39d47ae7216ac
Prajwal-18MD <prajju.18gryphon@gmail.com>
2024-03-09 16:25:21 +0530

    miner rewards

--- blockchain/client/sendBTC.py
+++ blockchain/client/sendBTC.py
@@ -1,5 +1,6 @@
 from Blockchain.Backend.util.util import decode_base58
 from Blockchain.Backend.core.Script import Script
+from Blockchain.Backend.core.Tx import Tx, TxIn, TxOut
 import time
 
 class SendBTC:
@@ -17,7 +18,7 @@ class SendBTC:
         
     def prepareTxIn(self):
         TxIns = []
-        self.Toatal = 0 
+        self.Total = 0 
         
         self.From_address_script_pubkey = self.scriptPubKey(self.FromPublicAddress)
         self.fromPubKeyHash = self.From_address_script_pubkey.cmds[2]
@@ -30,11 +31,38 @@ class SendBTC:
                 time.sleep(2)
         except Exception as e:
             print(f"Error in converting the Managed Dict to Normal Dict")
+            
+        for Txbyte in  newutxos:
+            if self.Total < self.Amount :
+                TxObj = newutxos[Txbyte]
+            
+                for index, txout in enumerate(TxObj.tx_outs):
+                    if txout.script_pubkey.cmds[2] == self.fromPubKeyHash:
+                        self.Total += txout.amount
+                        prev_tx = bytes.fromhex(TxObj.id())
+                        TxIns.append(TxIn(prev_tx ,index))
+            else:
+                break
+        
+        if self.Total < self.Amount :
+            self.isBalanceEnough = False
+        return TxIns
+            
+        
     
     def prepareTxOut(self):
-        pass
+        TxOuts = []
+        to_scriptPubkey = self.scriptPubKey(self.toAccount)
+        TxOuts.append(TxOut(self.Amount, to_scriptPubkey))
+
+        
+        self.fee = self.COIN
+        self.changeAmount = self.Total - self.Amount - self.fee
+        TxOuts.append(TxOut(self.changeAmount, self.From_address_script_pubkey))
+        return TxOuts
+        
         
     def prepareTransaction(self):
-        self.prepareTxIn()
-        self.prepareTxOut()
-        
+       self.TxIns = self.prepareTxIn()
+       self.TxOuts = self.prepareTxOut()
+       self.TxObj = Tx(1, self.TxIns, self.TxOuts, 0)
